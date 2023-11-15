b59a7892a082bedf183e46bd1d103dcdd505cd0e
Julian Lastra <Julian.Lastra@gds.ey.com>
2023-11-14 23:01:18 -0300

    Validate email and DNI created not implemented

---
+++ Vista/Customs/ValidateEmail.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ValidateEmail.ascx.cs" Inherits="Vista.Customs.ValidateEmail" %>
+
+<asp:TextBox class="customTextbox" ID="TextBox1" runat="server" OnTextChanged="tb_KeyDown"></asp:TextBox>
+<br />
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
---
+++ Vista/Customs/ValidateEmail.ascx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vista.Customs
+{
+    public partial class ValidateEmail : System.Web.UI.UserControl
+    {
+        public bool valid = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tb_KeyDown(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool isValid()
+        {
+            string email = TextBox1.Text;
+            string pattern = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+            Regex regex = new Regex(pattern);
+            valid = regex.IsMatch(email);
+            return valid;
+        }
+    }
+}
