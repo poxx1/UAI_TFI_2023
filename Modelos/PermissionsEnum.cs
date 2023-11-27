d9c5e3e119f459be360ac2806413560210488697
Julian Lastra <Julian.Lastra@gds.ey.com>
2023-11-27 17:20:59 -0300

    Permisos modificacion

--- Modelos/PermissionsEnum.cs
+++ Modelos/PermissionsEnum.cs
@@ -1,10 +1,16 @@
 ﻿namespace Modelos
 {
-        public enum PermissionsEnum
-        {
-            Default,
-            Patentes,
-            Usuarios,
-            Bitacora
-        }
+    public enum PermissionsEnum
+    {   
+        Default,
+        Patentes,
+        Usuarios,
+        Bitacora,
+        AdminCompras,
+        AdminSolicitudes,
+        Backup,
+        Solicitud,
+        WebService,
+        XML
+    }
 }
