cb9f041379bd5b2519ec5543dd9da82f37808a5a
Julian Lastra <Julian.Lastra@gds.ey.com>
2023-11-29 08:03:49 -0300

    Validar los campos con regex de Add y Edit user

--- Vista/AddUser.aspx.cs
+++ Vista/AddUser.aspx.cs
@@ -4,6 +4,8 @@ using Servicios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -20,26 +22,76 @@ namespace Vista
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            UserModel user = new UserModel();
-            user.Dni = TextBox1.Text;
-            user.Name = TextBox2.Text;
-            user.LastName = TextBox3.Text;
-            user.Nickname = TextBox4.Text;   
-            user.Password = TextBox5.Text;
-            user.Mail = TextBox6.Text;
-            user.Phone = TextBox7.Text;
-            user.Adress = TextBox8.Text;
+            bool isValidData = true;
 
-            if (AddUser(user))
+            #region Regex
+            if (!Regex.IsMatch(TextBox1.Text, "^([0-9]+$)")) //Numbers only DNI
             {
-                GlobalMessage.MessageBox(this, $"Se agrego a {user.Nickname} al sistema");
+                isValidData = false;
+                GlobalMessage.MessageBox(this, $"El DNI no es valido");
+            }
+            if (!Regex.IsMatch(TextBox2.Text, "^([a-zA-Z]+$)")) //Letters only NOMBRE
+            { isValidData = false; GlobalMessage.MessageBox(this, $"El nombre no es valido");
+            }
+            if (!Regex.IsMatch(TextBox3.Text, "^([a-zA-Z]+$)"))//Numbers only APELLIDO
+            { isValidData = false; GlobalMessage.MessageBox(this, $"El Apellido no es valido");
+            }
+            if (!Regex.IsMatch(TextBox3.Text, "^([a-zA-Z]+$)"))//Letters only NICK
+            { isValidData = false; GlobalMessage.MessageBox(this, $"El Nickname no es valido, solo letras");
+            }
+            int count = TextBox3.Text.Split('/').Length - 1;
+            if (count<8)//(!Regex.IsMatch(TextBox3.Text, "^(\\s*(\\S)\\s*){8,}\r\n"))//PASSWORD > 8
+            { isValidData = false; GlobalMessage.MessageBox(this, $"La password no es valida, mayor a 8 caracteres");
+            }
+            if (!isValidEmail(TextBox6.Text))//EMAIL
+            { isValidData = false; GlobalMessage.MessageBox(this, $"El email no es valido");
+            }
+            if (!Regex.IsMatch(TextBox7.Text, "^([0-9]+$)"))//Numbers only - Telefono
+            { isValidData = false; GlobalMessage.MessageBox(this, $"El telefono no es valido");
+            }
+            if (TextBox8.Text == "")//fijate que no te vacio
+            { isValidData = false; GlobalMessage.MessageBox(this, $"La direccion no es valida");
+            }
+            #endregion
+            if (isValidData)
+            {
+                UserModel user = new UserModel();
+                user.Dni = TextBox1.Text;
+                user.Name = TextBox2.Text;
+                user.LastName = TextBox3.Text;
+                user.Nickname = TextBox4.Text;
+                user.Password = TextBox5.Text;
+                user.Mail = TextBox6.Text;
+                user.Phone = TextBox7.Text;
+                user.Adress = TextBox8.Text;
+
+                if (AddUser(user))
+                {
+                    GlobalMessage.MessageBox(this, $"Se agrego a {user.Nickname} al sistema");
 
 
-                BitacoraService bitacoraService = new BitacoraService();
-                bitacoraService.LogData("Login", $"El usuario {user.Name} agrego un usuario nuevo.", "Media");
+                    BitacoraService bitacoraService = new BitacoraService();
+                    bitacoraService.LogData("Login", $"El usuario {user.Name} agrego un usuario nuevo.", "Media");
+                }
+                else
+                    GlobalMessage.MessageBox(this, $"No se pudo agregar a {user.Nickname} al sistema");
             }
             else
-                GlobalMessage.MessageBox(this, $"No se pudo agregar a {user.Nickname} al sistema");
+                GlobalMessage.MessageBox(this, $"Ingreso un campo invalido. Por favor revisar.");
+        }
+
+        public bool isValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
         }
 
         public bool AddUser(UserModel user) {
--- Vista/EditUser.aspx.cs
+++ Vista/EditUser.aspx.cs
@@ -2,6 +2,8 @@
 using Model;
 using Servicios;
 using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Vista
@@ -22,29 +24,86 @@ namespace Vista
                 }
             }
         }
+        public bool isValidEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
         protected void Button1_Click(object sender, EventArgs e)
         {
-            string userName = ListBox1.Text;
+            bool isValidData = true;
 
-            UserModel usuario = us.Get(userName);
-            usuario.Dni = TextBox1.Text;
-            usuario.Name = TextBox2.Text;
-            usuario.LastName = TextBox3.Text;
-            usuario.Nickname = TextBox4.Text;
-            usuario.Mail = TextBox6.Text;
-            usuario.Phone = TextBox7.Text;
-            usuario.Adress = TextBox8.Text;
-
-            if (TextBox5.Text.Length > 0) { usuario.Password = Security.HashSha256(TextBox5.Text); }
-            if (us.UpdateUser(usuario))
+            #region Regex
+            if (!Regex.IsMatch(TextBox1.Text, "^([0-9]+$)")) //Numbers only DNI
             {
-                BitacoraService bitacoraService = new BitacoraService();
-                UserModel user = new UserModel();
-                bitacoraService.LogData("Login", $"El usuario {user.Name} edito un curso.", "Media");
-                GlobalMessage.MessageBox(this, $"Se edito a {userName}");
+                isValidData = false;
+                GlobalMessage.MessageBox(this, $"El DNI no es valido");
+            }
+            if (!Regex.IsMatch(TextBox2.Text, "^([a-zA-Z]+$)")) //Letters only NOMBRE
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"El nombre no es valido");
+            }
+            if (!Regex.IsMatch(TextBox3.Text, "^([a-zA-Z]+$)"))//Numbers only APELLIDO
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"El Apellido no es valido");
+            }
+            if (!Regex.IsMatch(TextBox3.Text, "^([a-zA-Z]+$)"))//Letters only NICK
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"El Nickname no es valido, solo letras");
+            }
+            int count = TextBox3.Text.Split('/').Length - 1;
+            if (count < 8)//(!Regex.IsMatch(TextBox3.Text, "^(\\s*(\\S)\\s*){8,}\r\n"))//PASSWORD > 8
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"La password no es valida, mayor a 8 caracteres");
+            }
+            if (!isValidEmail(TextBox6.Text))//EMAIL
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"El email no es valido");
+            }
+            if (!Regex.IsMatch(TextBox7.Text, "^([0-9]+$)"))//Numbers only - Telefono
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"El telefono no es valido");
+            }
+            if (TextBox8.Text == "")//fijate que no te vacio
+            {
+                isValidData = false; GlobalMessage.MessageBox(this, $"La direccion no es valida");
+            }
+            #endregion
+            if (isValidData)
+            { 
+                string userName = ListBox1.Text;
+
+                UserModel usuario = us.Get(userName);
+                usuario.Dni = TextBox1.Text;
+                usuario.Name = TextBox2.Text;
+                usuario.LastName = TextBox3.Text;
+                usuario.Nickname = TextBox4.Text;
+                usuario.Mail = TextBox6.Text;
+                usuario.Phone = TextBox7.Text;
+                usuario.Adress = TextBox8.Text;
+
+                if (TextBox5.Text.Length > 0) { usuario.Password = Security.HashSha256(TextBox5.Text); }
+                if (us.UpdateUser(usuario))
+                {
+                    BitacoraService bitacoraService = new BitacoraService();
+                    UserModel user = new UserModel();
+                    bitacoraService.LogData("Login", $"El usuario {user.Name} edito un curso.", "Media");
+                    GlobalMessage.MessageBox(this, $"Se edito a {userName}");
+                }
+                else
+                    GlobalMessage.MessageBox(this, $"No se pudo editar a {userName}");
             }
             else
-                GlobalMessage.MessageBox(this, $"No se pudo editar a {userName}");
+                GlobalMessage.MessageBox(this, $"Ingreso un campo invalido. Por favor revisar.");
         }
         protected void Button2_Click(object sender, EventArgs e)
         {
