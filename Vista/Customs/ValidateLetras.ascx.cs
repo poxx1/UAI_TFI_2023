c22b20faf8d4ed7d7a53a913090a21fa0727817a
Julian Lastra <Julian.Lastra@gds.ey.com>
2023-11-29 15:39:13 -0300

    Validations

---
+++ Vista/Customs/ValidateLetras.ascx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vista.Customs
+{
+    public partial class ValidateLetras : System.Web.UI.UserControl
+    {
+        public bool valid = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void tb_KeyDown(object sender, EventArgs e)
+        {
+        
+        }
+        public bool isValid()
+        {
+            string email = TextBox1.Text;
+            Regex regex = new Regex(@"^\d$");
+            valid = regex.IsMatch(email);
+            return valid;
+        }
+    }
+}
---
+++ Vista/Customs/ValidateNumerico.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ValidateNumerico.ascx.cs" Inherits="Vista.Customs.ValidateNumerico" %>
+
+<asp:TextBox class="customTextbox" ID="TextBox1" runat="server" OnTextChanged="tb_KeyDown"></asp:TextBox>
+<br />
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
